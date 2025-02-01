1c65ba11ab5554eeeffd35a51abfe4fa53dabcad
ptzecher <ptzecher@csd.auth.gr>
2025-02-01 02:30:20 +0200

    Minor changes

--- src/main/java/com/example/demo/controllers/MessageController.java
+++ src/main/java/com/example/demo/controllers/MessageController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Message;
 import com.example.demo.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
